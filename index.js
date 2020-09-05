ed017c951e6a32b8928e7d8d668c9f438b571487
Ates Balci <atesbalci@gmail.com>
2020-09-05 19:21:41 +0300

    Added dolar bey bot

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const { startBot } = require('./HeratBot/bot.js');
+const { startDolarBot } = require('./dolar-bey/bot.js');
 
 startBot(process.env.HERATBOT_BOT_TOKEN);
+startDolarBot(process.env.DOLARBEY_BOT_TOKEN);
--- runner_template.sh
+++ runner_template.sh
@@ -1,6 +1,8 @@
 #Clone and rename this to runner.sh
 
 HERATBOT_BOT_TOKEN='bot token' \
+DOLARBEY_BOT_TOKEN='bot token' \
+RECORD_CHANNEL_ID='record channel id' \
 GIT_USER='git user for pull and commit' \
 GIT_PASS='git password' \
 node index.js
