e353910e56b56ee0c9f7b7cdb3c955d614931dd4
Ates Balci <atesbalci@gmail.com>
2020-09-06 12:13:25 +0300

    Added new dolar bey telegram bot

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { startBot } = require('./HeratBot/bot.js');
 const { startDolarBot } = require('./dolar-bey/bot.js');
+const express = require('express');
+const { prepareDolarTelegramExpress } = require('./dolar-bey/telegram_bot.js');
+const app = express();
+
+function startDolarTelegramServer() {
+  app.use(express.json());
+  prepareDolarTelegramExpress(app);
+  app.listen(process.env.PORT || 80, () => console.log('Listening...'));
+}
 
 startBot(process.env.HERATBOT_BOT_TOKEN);
 startDolarBot(process.env.DOLARBEY_BOT_TOKEN);
+startDolarTelegramServer();
--- runner_template.sh
+++ runner_template.sh
@@ -5,4 +5,7 @@ DOLARBEY_BOT_TOKEN='bot token' \
 RECORD_CHANNEL_ID='record channel id' \
 GIT_USER='git user for pull and commit' \
 GIT_PASS='git password' \
+DOLAR_TELEGRAM_GROUP_URL_PREFIX='url prefix' \
+DOLAR_TELEGRAM_GROUP_ID='group id' \
+DOLAR_RECORD_TOLERANCE='0.002' \
 node index.js
