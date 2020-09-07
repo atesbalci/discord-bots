fe6f6bfdc5fd8472414db528ccc6b353669011c6
Ates Balci <atesbalci@gmail.com>
2020-09-08 01:36:30 +0300

    Added corona bot

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const { startBot } = require('./HeratBot/bot.js');
 const { startDolarBot } = require('./dolar-bey/bot.js');
 const express = require('express');
-const { prepareDolarTelegramExpress } = require('./dolar-bey/telegram_bot.js');
+const { handleDolarTelegram, initDolarTelegram } = require('./dolar-bey/telegram_bot.js');
+const { handleCoronaTelegram, initCoronaTelegram } = require('./corona-hanim/telegram_bot.js');
 const app = express();
 
-function startDolarTelegramServer() {
+function startServer() {
   app.use(express.json());
-  prepareDolarTelegramExpress(app);
+  app.post('/', (req, res) => {    
+    res.send('All done!');
+    handleCoronaTelegram(req, res);
+    handleDolarTelegram(req, res);
+  });
   app.listen(process.env.PORT || 80, () => console.log('Listening...'));
 }
 
 startBot(process.env.HERATBOT_BOT_TOKEN);
 startDolarBot(process.env.DOLARBEY_BOT_TOKEN);
-startDolarTelegramServer();
+initCoronaTelegram();
+initDolarTelegram();
+startServer();
