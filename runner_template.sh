8a17b0990244aa880e56899caca6168f8f03dd0d
Ates Balci <atesbalci@gmail.com>
2020-09-13 00:31:37 +0300

    HeratMusik update

--- runner_template.sh
+++ runner_template.sh
@@ -11,4 +11,5 @@ DOLAR_TELEGRAM_GROUP_ID='group id' \
 DOLAR_RECORD_TOLERANCE='0.002' \
 CORONA_TELEGRAM_GROUP_ID='group_id' \
 CORONA_TELEGRAM_SEND_URL_PREFIX='url prefix' \
+YOUTUBE_API_KEY='youtube api key' \
 node index.js
